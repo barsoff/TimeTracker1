7f11331dd3d366690dc14853ab00ec4b04dbb5b0
Марсель Махмутов <barsoff_21@mail.ru>
2023-03-29 23:09:28 +0300

    Добавил дизайн для формы регистрации

--- TimeTracker1/FormRegister.Designer.cs
+++ TimeTracker1/FormRegister.Designer.cs
@@ -57,17 +57,17 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Constantia", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label1.Location = new System.Drawing.Point(42, 23);
+            this.label1.Font = new System.Drawing.Font("Consolas", 20F, System.Drawing.FontStyle.Bold);
+            this.label1.Location = new System.Drawing.Point(88, 61);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(602, 37);
+            this.label1.Size = new System.Drawing.Size(701, 40);
             this.label1.TabIndex = 0;
             this.label1.Text = "Зарегистрировать нового пользователя";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // panel1
             // 
-            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(55)))), ((int)(((byte)(215)))), ((int)(((byte)(138)))));
             this.panel1.Controls.Add(this.buttonClearAll);
             this.panel1.Controls.Add(this.textBox5);
             this.panel1.Controls.Add(this.label5);
@@ -88,19 +88,19 @@
             this.panel1.Controls.Add(this.label3);
             this.panel1.Controls.Add(this.label2);
             this.panel1.Controls.Add(this.label1);
-            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Location = new System.Drawing.Point(-1, -2);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(611, 517);
+            this.panel1.Size = new System.Drawing.Size(757, 695);
             this.panel1.TabIndex = 2;
             // 
             // buttonClearAll
             // 
             this.buttonClearAll.Cursor = System.Windows.Forms.Cursors.Hand;
             this.buttonClearAll.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.buttonClearAll.Font = new System.Drawing.Font("Constantia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.buttonClearAll.Location = new System.Drawing.Point(348, 439);
+            this.buttonClearAll.Font = new System.Drawing.Font("Consolas", 12F);
+            this.buttonClearAll.Location = new System.Drawing.Point(52, 569);
             this.buttonClearAll.Name = "buttonClearAll";
-            this.buttonClearAll.Size = new System.Drawing.Size(195, 35);
+            this.buttonClearAll.Size = new System.Drawing.Size(270, 50);
             this.buttonClearAll.TabIndex = 24;
             this.buttonClearAll.Text = "Очистить все поля";
             this.buttonClearAll.UseVisualStyleBackColor = true;
@@ -111,31 +111,32 @@
             this.textBox5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.textBox5.Font = new System.Drawing.Font("Constantia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.textBox5.ForeColor = System.Drawing.SystemColors.WindowText;
-            this.textBox5.Location = new System.Drawing.Point(317, 281);
+            this.textBox5.Location = new System.Drawing.Point(409, 447);
+            this.textBox5.Multiline = true;
             this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(250, 32);
+            this.textBox5.Size = new System.Drawing.Size(282, 32);
             this.textBox5.TabIndex = 23;
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Font = new System.Drawing.Font("Constantia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label5.Location = new System.Drawing.Point(313, 259);
+            this.label5.Font = new System.Drawing.Font("Consolas", 15F);
+            this.label5.Location = new System.Drawing.Point(502, 415);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(102, 24);
+            this.label5.Size = new System.Drawing.Size(111, 29);
             this.label5.TabIndex = 22;
-            this.label5.Text = "Телефон*:";
+            this.label5.Text = "Телефон";
             this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Font = new System.Drawing.Font("Constantia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label6.Location = new System.Drawing.Point(313, 326);
+            this.label6.Font = new System.Drawing.Font("Consolas", 15F);
+            this.label6.Location = new System.Drawing.Point(421, 513);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(205, 24);
+            this.label6.Size = new System.Drawing.Size(251, 29);
             this.label6.TabIndex = 21;
-            this.label6.Text = "Электронная почта*:";
+            this.label6.Text = "Электронная почта";
             this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // textBox6
@@ -143,9 +144,10 @@
             this.textBox6.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.textBox6.Font = new System.Drawing.Font("Constantia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.textBox6.ForeColor = System.Drawing.SystemColors.WindowText;
-            this.textBox6.Location = new System.Drawing.Point(317, 348);
+            this.textBox6.Location = new System.Drawing.Point(409, 545);
+            this.textBox6.Multiline = true;
             this.textBox6.Name = "textBox6";
-            this.textBox6.Size = new System.Drawing.Size(250, 32);
+            this.textBox6.Size = new System.Drawing.Size(282, 32);
             this.textBox6.TabIndex = 20;
             // 
             // textBox8
@@ -153,20 +155,21 @@
             this.textBox8.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.textBox8.Font = new System.Drawing.Font("Constantia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.textBox8.ForeColor = System.Drawing.SystemColors.WindowText;
-            this.textBox8.Location = new System.Drawing.Point(317, 84);
+            this.textBox8.Location = new System.Drawing.Point(409, 163);
+            this.textBox8.Multiline = true;
             this.textBox8.Name = "textBox8";
-            this.textBox8.Size = new System.Drawing.Size(250, 32);
+            this.textBox8.Size = new System.Drawing.Size(282, 32);
             this.textBox8.TabIndex = 19;
             // 
             // label9
             // 
             this.label9.AutoSize = true;
-            this.label9.Font = new System.Drawing.Font("Constantia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label9.Location = new System.Drawing.Point(313, 62);
+            this.label9.Font = new System.Drawing.Font("Consolas", 15F);
+            this.label9.Location = new System.Drawing.Point(492, 131);
             this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(107, 24);
+            this.label9.Size = new System.Drawing.Size(111, 29);
             this.label9.TabIndex = 18;
-            this.label9.Text = "Фамилия*:";
+            this.label9.Text = "Фамилия";
             this.label9.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // textBox7
@@ -174,31 +177,32 @@
             this.textBox7.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.textBox7.Font = new System.Drawing.Font("Constantia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.textBox7.ForeColor = System.Drawing.SystemColors.WindowText;
-            this.textBox7.Location = new System.Drawing.Point(317, 149);
+            this.textBox7.Location = new System.Drawing.Point(409, 259);
+            this.textBox7.Multiline = true;
             this.textBox7.Name = "textBox7";
-            this.textBox7.Size = new System.Drawing.Size(250, 32);
+            this.textBox7.Size = new System.Drawing.Size(282, 32);
             this.textBox7.TabIndex = 17;
             // 
             // label8
             // 
             this.label8.AutoSize = true;
-            this.label8.Font = new System.Drawing.Font("Constantia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label8.Location = new System.Drawing.Point(313, 127);
+            this.label8.Font = new System.Drawing.Font("Consolas", 15F);
+            this.label8.Location = new System.Drawing.Point(522, 227);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(64, 24);
+            this.label8.Size = new System.Drawing.Size(55, 29);
             this.label8.TabIndex = 16;
-            this.label8.Text = "Имя*:";
+            this.label8.Text = "Имя";
             this.label8.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Font = new System.Drawing.Font("Constantia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label7.Location = new System.Drawing.Point(313, 194);
+            this.label7.Font = new System.Drawing.Font("Consolas", 15F);
+            this.label7.Location = new System.Drawing.Point(492, 322);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(111, 24);
+            this.label7.Size = new System.Drawing.Size(125, 29);
             this.label7.TabIndex = 14;
-            this.label7.Text = "Отчество*:";
+            this.label7.Text = "Отчество";
             this.label7.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // textBox4
@@ -206,9 +210,10 @@
             this.textBox4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.textBox4.Font = new System.Drawing.Font("Constantia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.textBox4.ForeColor = System.Drawing.SystemColors.WindowText;
-            this.textBox4.Location = new System.Drawing.Point(317, 216);
+            this.textBox4.Location = new System.Drawing.Point(409, 354);
+            this.textBox4.Multiline = true;
             this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(250, 32);
+            this.textBox4.Size = new System.Drawing.Size(282, 32);
             this.textBox4.TabIndex = 11;
             // 
             // textBox3
@@ -216,31 +221,32 @@
             this.textBox3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.textBox3.Font = new System.Drawing.Font("Constantia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.textBox3.ForeColor = System.Drawing.SystemColors.WindowText;
-            this.textBox3.Location = new System.Drawing.Point(28, 281);
+            this.textBox3.Location = new System.Drawing.Point(52, 389);
+            this.textBox3.Multiline = true;
             this.textBox3.Name = "textBox3";
             this.textBox3.PasswordChar = '*';
-            this.textBox3.Size = new System.Drawing.Size(250, 32);
+            this.textBox3.Size = new System.Drawing.Size(270, 32);
             this.textBox3.TabIndex = 9;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Constantia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label4.Location = new System.Drawing.Point(24, 259);
+            this.label4.Font = new System.Drawing.Font("Consolas", 15F);
+            this.label4.Location = new System.Drawing.Point(70, 357);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(193, 24);
+            this.label4.Size = new System.Drawing.Size(237, 29);
             this.label4.TabIndex = 8;
-            this.label4.Text = "Повторите пароль*:";
+            this.label4.Text = "Повторите пароль";
             this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // buttonRegister
             // 
             this.buttonRegister.Cursor = System.Windows.Forms.Cursors.Hand;
             this.buttonRegister.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.buttonRegister.Font = new System.Drawing.Font("Constantia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.buttonRegister.Location = new System.Drawing.Point(348, 389);
+            this.buttonRegister.Font = new System.Drawing.Font("Consolas", 12F);
+            this.buttonRegister.Location = new System.Drawing.Point(52, 513);
             this.buttonRegister.Name = "buttonRegister";
-            this.buttonRegister.Size = new System.Drawing.Size(195, 35);
+            this.buttonRegister.Size = new System.Drawing.Size(270, 50);
             this.buttonRegister.TabIndex = 6;
             this.buttonRegister.Text = "Зарегистрироваться";
             this.buttonRegister.UseVisualStyleBackColor = true;
@@ -251,10 +257,11 @@
             this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.textBox2.Font = new System.Drawing.Font("Constantia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.textBox2.ForeColor = System.Drawing.SystemColors.WindowText;
-            this.textBox2.Location = new System.Drawing.Point(28, 213);
+            this.textBox2.Location = new System.Drawing.Point(52, 289);
+            this.textBox2.Multiline = true;
             this.textBox2.Name = "textBox2";
             this.textBox2.PasswordChar = '*';
-            this.textBox2.Size = new System.Drawing.Size(250, 32);
+            this.textBox2.Size = new System.Drawing.Size(270, 32);
             this.textBox2.TabIndex = 5;
             // 
             // textBox1
@@ -262,19 +269,20 @@
             this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.textBox1.Font = new System.Drawing.Font("Constantia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.textBox1.ForeColor = System.Drawing.SystemColors.WindowText;
-            this.textBox1.Location = new System.Drawing.Point(28, 149);
+            this.textBox1.Location = new System.Drawing.Point(52, 198);
+            this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(250, 32);
+            this.textBox1.Size = new System.Drawing.Size(270, 32);
             this.textBox1.TabIndex = 4;
             // 
             // labelGoToLogForm
             // 
             this.labelGoToLogForm.AutoSize = true;
             this.labelGoToLogForm.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.labelGoToLogForm.Font = new System.Drawing.Font("Constantia", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.labelGoToLogForm.Location = new System.Drawing.Point(355, 488);
+            this.labelGoToLogForm.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Italic);
+            this.labelGoToLogForm.Location = new System.Drawing.Point(493, 664);
             this.labelGoToLogForm.Name = "labelGoToLogForm";
-            this.labelGoToLogForm.Size = new System.Drawing.Size(238, 21);
+            this.labelGoToLogForm.Size = new System.Drawing.Size(296, 23);
             this.labelGoToLogForm.TabIndex = 3;
             this.labelGoToLogForm.Text = "Перейти к окну авторизации";
             this.labelGoToLogForm.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -283,23 +291,23 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Constantia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label3.Location = new System.Drawing.Point(24, 191);
+            this.label3.Font = new System.Drawing.Font("Consolas", 15F);
+            this.label3.Location = new System.Drawing.Point(136, 257);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(96, 24);
+            this.label3.Size = new System.Drawing.Size(97, 29);
             this.label3.TabIndex = 2;
-            this.label3.Text = "Пароль*: ";
+            this.label3.Text = "Пароль";
             this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Constantia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label2.Location = new System.Drawing.Point(24, 127);
+            this.label2.Font = new System.Drawing.Font("Consolas", 15F);
+            this.label2.Location = new System.Drawing.Point(136, 166);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(82, 24);
+            this.label2.Size = new System.Drawing.Size(83, 29);
             this.label2.TabIndex = 1;
-            this.label2.Text = "Логин*:";
+            this.label2.Text = "Логин";
             this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // FormRegister
@@ -307,7 +315,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 17F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.WhiteSmoke;
-            this.ClientSize = new System.Drawing.Size(635, 539);
+            this.ClientSize = new System.Drawing.Size(756, 694);
             this.Controls.Add(this.panel1);
             this.Font = new System.Drawing.Font("Constantia", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
