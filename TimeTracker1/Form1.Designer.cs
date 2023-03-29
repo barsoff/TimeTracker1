c8ac8d3a132d9d46143e7211a9165861757b5fbd
Марсель Махмутов <barsoff_21@mail.ru>
2023-03-29 23:08:17 +0300

    Добавил дизайн для формы авторизации

--- TimeTracker1/Form1.Designer.cs
+++ TimeTracker1/Form1.Designer.cs
@@ -37,23 +37,25 @@
             this.labelGoToRegForm = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // label1
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Constantia", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label1.Location = new System.Drawing.Point(42, 23);
+            this.label1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label1.Font = new System.Drawing.Font("Consolas", 26F, System.Drawing.FontStyle.Bold);
+            this.label1.Location = new System.Drawing.Point(0, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(257, 37);
+            this.label1.Size = new System.Drawing.Size(487, 148);
             this.label1.TabIndex = 0;
-            this.label1.Text = "Войти в аккаунт";
+            this.label1.Text = "Авторизация";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // panel1
             // 
-            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(55)))), ((int)(((byte)(215)))), ((int)(((byte)(138)))));
+            this.panel1.Controls.Add(this.label4);
             this.panel1.Controls.Add(this.labelForgotPass);
             this.panel1.Controls.Add(this.buttonLogin);
             this.panel1.Controls.Add(this.textBox2);
@@ -62,31 +64,35 @@
             this.panel1.Controls.Add(this.label3);
             this.panel1.Controls.Add(this.label2);
             this.panel1.Controls.Add(this.label1);
-            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.ForeColor = System.Drawing.Color.Black;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(290, 252);
+            this.panel1.Size = new System.Drawing.Size(487, 478);
             this.panel1.TabIndex = 1;
             // 
             // labelForgotPass
             // 
             this.labelForgotPass.AutoSize = true;
             this.labelForgotPass.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.labelForgotPass.Font = new System.Drawing.Font("Constantia", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.labelForgotPass.Location = new System.Drawing.Point(186, 228);
+            this.labelForgotPass.Font = new System.Drawing.Font("Consolas", 10.8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelForgotPass.Location = new System.Drawing.Point(361, 447);
             this.labelForgotPass.Name = "labelForgotPass";
-            this.labelForgotPass.Size = new System.Drawing.Size(131, 21);
+            this.labelForgotPass.Size = new System.Drawing.Size(150, 22);
             this.labelForgotPass.TabIndex = 7;
             this.labelForgotPass.Text = "Забыли пароль?";
             this.labelForgotPass.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // buttonLogin
             // 
+            this.buttonLogin.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
             this.buttonLogin.Cursor = System.Windows.Forms.Cursors.Hand;
             this.buttonLogin.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.buttonLogin.Font = new System.Drawing.Font("Constantia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.buttonLogin.Location = new System.Drawing.Point(103, 189);
+            this.buttonLogin.Font = new System.Drawing.Font("Consolas", 16F);
+            this.buttonLogin.ForeColor = System.Drawing.Color.Black;
+            this.buttonLogin.Location = new System.Drawing.Point(148, 357);
             this.buttonLogin.Name = "buttonLogin";
-            this.buttonLogin.Size = new System.Drawing.Size(75, 25);
+            this.buttonLogin.Size = new System.Drawing.Size(190, 56);
             this.buttonLogin.TabIndex = 6;
             this.buttonLogin.Text = "Войти";
             this.buttonLogin.UseVisualStyleBackColor = true;
@@ -97,9 +103,10 @@
             this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.textBox2.Font = new System.Drawing.Font("Constantia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.textBox2.ForeColor = System.Drawing.SystemColors.WindowText;
-            this.textBox2.Location = new System.Drawing.Point(20, 148);
+            this.textBox2.Location = new System.Drawing.Point(99, 284);
+            this.textBox2.Multiline = true;
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(250, 32);
+            this.textBox2.Size = new System.Drawing.Size(282, 40);
             this.textBox2.TabIndex = 5;
             // 
             // textBox1
@@ -107,19 +114,20 @@
             this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.textBox1.Font = new System.Drawing.Font("Constantia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.textBox1.ForeColor = System.Drawing.SystemColors.WindowText;
-            this.textBox1.Location = new System.Drawing.Point(20, 84);
+            this.textBox1.Location = new System.Drawing.Point(99, 184);
+            this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(250, 32);
-            this.textBox1.TabIndex = 4;
+            this.textBox1.Size = new System.Drawing.Size(282, 40);
+            this.textBox1.TabIndex = 5;
             // 
             // labelGoToRegForm
             // 
             this.labelGoToRegForm.AutoSize = true;
             this.labelGoToRegForm.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.labelGoToRegForm.Font = new System.Drawing.Font("Constantia", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.labelGoToRegForm.Location = new System.Drawing.Point(3, 228);
+            this.labelGoToRegForm.Font = new System.Drawing.Font("Consolas", 10.8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelGoToRegForm.Location = new System.Drawing.Point(12, 447);
             this.labelGoToRegForm.Name = "labelGoToRegForm";
-            this.labelGoToRegForm.Size = new System.Drawing.Size(210, 21);
+            this.labelGoToRegForm.Size = new System.Drawing.Size(240, 22);
             this.labelGoToRegForm.TabIndex = 3;
             this.labelGoToRegForm.Text = "Еще нет учетной записи?";
             this.labelGoToRegForm.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -127,32 +135,40 @@
             // 
             // label3
             // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Constantia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label3.Location = new System.Drawing.Point(16, 126);
+            this.label3.Font = new System.Drawing.Font("Consolas", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(0, 227);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(82, 24);
+            this.label3.Size = new System.Drawing.Size(487, 54);
             this.label3.TabIndex = 2;
-            this.label3.Text = "Пароль:";
+            this.label3.Text = "Пароль";
             this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // label2
             // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Constantia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label2.Location = new System.Drawing.Point(16, 62);
+            this.label2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(55)))), ((int)(((byte)(215)))), ((int)(((byte)(138)))));
+            this.label2.Font = new System.Drawing.Font("Consolas", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(-3, 127);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(73, 24);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "Логин:";
+            this.label2.Size = new System.Drawing.Size(487, 54);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Логин";
             this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Georgia", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(453, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 32);
+            this.label4.TabIndex = 8;
             // 
             // FormAuth
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 17F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.WhiteSmoke;
-            this.ClientSize = new System.Drawing.Size(314, 273);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(227)))), ((int)(((byte)(188)))), ((int)(((byte)(61)))));
+            this.ClientSize = new System.Drawing.Size(487, 478);
             this.Controls.Add(this.panel1);
             this.Font = new System.Drawing.Font("Constantia", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
@@ -178,5 +194,6 @@
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label labelForgotPass;
+        private System.Windows.Forms.Label label4;
     }
 }
