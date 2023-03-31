08b23d6176114c21e5ecb3eb97a0a94d870c6bc9
Марсель Махмутов <barsoff_21@mail.ru>
2023-03-31 22:19:09 +0300

    Регистрация пользователя с отправкой данных в БД

--- TimeTracker1/FormRegister.cs
+++ TimeTracker1/FormRegister.cs
@@ -5,16 +5,20 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.Configuration;
-using System.Data.SqlClient;
-using Npgsql;
+using TimeTracker1.DataBase;
+using TimeTracker1.AuthClasses;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace TimeTracker1
 {
     public partial class FormRegister : Form
     {
+
+        private ClassDataBase database;
+
         private bool isClose = true; // Флаг для того, чтобы приложение не закрывалось при закрытии формы
         public FormRegister()
         {
@@ -39,24 +43,42 @@ namespace TimeTracker1
 
         private void buttonRegister_Click(object sender, EventArgs e)
         {
-            // connect to server PostgreSQL
-            NpgsqlConnection connect = new NpgsqlConnection ("Host = localhost; Port = 5432; Database = TimeTracker_DateBase; " +
-            "Username = postgres; Password = barsoff_21");
-            connect.Open();
-
-            // перенос данных в схемы autorization и public
-            var commandAutorization = new NpgsqlCommand($"insert into autorization.user (firstname, lastname, middlename, phone, email, disabled, login, password) values ('{textBox7.Text}', '{textBox8.Text}', '{textBox4.Text}', '{textBox5.Text}','{textBox6.Text}',false,'{textBox1.Text}','{textBox2.Text}')", connect);
-            var commandPublic = new NpgsqlCommand($"insert into public.user (first_name, last_name, middle_name, phone, email, disabled, login, password) values ('{textBox7.Text}', '{textBox8.Text}', '{textBox4.Text}', '{textBox5.Text}','{textBox6.Text}',false,'{textBox1.Text}','{textBox2.Text}')", connect);
-
-            commandAutorization.ExecuteNonQuery();
-            commandPublic.ExecuteNonQuery();
-            //MessageBox.Show(commandAutorization.ExecuteNonQuery().ToString());
-            //MessageBox.Show(commandPublic.ExecuteNonQuery().ToString());
-            connect.Close();
-
-            MessageBox.Show("\n   Вы успешно зарегистрированы   \n\n");
+            string script = "";
+            if (string.IsNullOrEmpty(textBox2.Text) || string.IsNullOrEmpty(textBox3.Text) || string.IsNullOrEmpty(textBox1.Text) || string.IsNullOrEmpty(textBox4.Text) || string.IsNullOrEmpty(textBox5.Text) || string.IsNullOrEmpty(textBox6.Text) || string.IsNullOrEmpty(textBox7.Text) || string.IsNullOrEmpty(textBox8.Text))
+            {
+                MessageBox.Show("Заполните все поля!");
+                return;
+            }
+            if (textBox2.Text != textBox3.Text)
+            {
+                MessageBox.Show("Пароли не совпадают!");
+                return;
+            }
+            if (!Regex.IsMatch(textBox6.Text, ".+@.+\\.[a-z]+"))
+            {
+                MessageBox.Show("Электронная почта введена в неверном формате");
+                return;
+            }
+            var res = database.SelectFunctionUsing("autorization.\"user\" u where '" + textBox1.Text.Trim() + "' = u.login");
+            res.Read();
+            if (res.HasRows)
+            {
+                MessageBox.Show("Пользователь с данным логином существует!");
+                res.Close();
+                return;
+            }
+            res.Close();
+            script += "do $$ declare v_iduser int8; begin insert into autorization.user (firstname, lastname, middlename, phone, email,disabled, login, password) values ('" + textBox7.Text.Trim() + "','" + textBox8.Text.Trim() + "', '" + textBox4.Text.Trim() + "', '" + textBox5.Text.Trim() + "', '" + textBox6.Text.Trim() + "',true, '" + textBox1.Text.Trim() + "', '" + textBox2.Text.Trim() + "') returning user_id into v_iduser;";
+            script += "insert into public.user (first_name, last_name, middle_name, phone, email, disabled, login, password) values('" + textBox7.Text.Trim() + "','" + textBox8.Text.Trim() + "', '" + textBox4.Text.Trim() + "', '" + textBox5.Text.Trim() + "', '" + textBox6.Text.Trim() + "', true, '" + textBox1.Text.Trim() + "', '" + textBox2.Text.Trim() + "');";
+            script += "insert into autorization.connect_user_role (user_id, role_id) values (v_iduser, 2);";
+            script += "insert into public.connect_user_role (user_id, role_id) values (v_iduser, 2);";
+            script += "end;$$";
+            if (!string.IsNullOrEmpty(script))
+            {
+                database.ExecuteScript(script);
+                MessageBox.Show("Пользователь создан!");
+            }
         }
-
         private void buttonClearAll_Click(object sender, EventArgs e)
         {
             textBox1.Clear();
@@ -67,6 +89,10 @@ namespace TimeTracker1
             textBox6.Clear();
             textBox7.Clear();
             textBox8.Clear();
+        }
+        public void SetDB(ClassDataBase _db)
+        {
+            this.database = _db;
         }
 
     }
